2eae85a57e7a318016052a69156f44acdd8af13e
jevliu <jevliu@163.com>
2023-07-01 14:22:30 +0800

    重构run_time()方法

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,20 +21,31 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
+    # 方法重构，添加辅助方法
+    def _check_events(self):
+        """响应案件和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    # 将更新屏幕的方法从run_game中转移到_update_screen()方法中
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            # 监视键盘和鼠标事件,使用辅助方法
+            self._check_events()
 
             # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._update_screen()
 
 
 if __name__ == "__main__":
